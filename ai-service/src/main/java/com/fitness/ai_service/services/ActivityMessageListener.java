5bb48b2ccbb0e5743c9e320f77c61cb2a62b85b1
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-31 12:29:53 +0530

    feat: receiving message via kafka done

---
+++ ai-service/src/main/java/com/fitness/ai_service/services/ActivityMessageListener.java
@@ -0,0 +1,18 @@
+package com.fitness.ai_service.services;
+
+import com.fitness.ai_service.models.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+
+    @KafkaListener(topics = "${kafka.topic.name}", groupId = "activity-processor-group")
+    public void processActivity(Activity activity) {
+        log.info("Received Activity for processing: {}", activity.getUserId());
+    }
+}
